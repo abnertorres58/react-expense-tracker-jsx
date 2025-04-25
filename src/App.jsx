a338a5db21b09d93b9710c33dca82204edbc6ea9
unknown <abnertorres58@gmail.com>
2025-04-25 19:30:59 -0400

    Initial commit of minimal JSX Tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // You can delete this if you're not using it
+
+function App() {
+    return (
+        <div className="bg-red-500 p-5 text-white">
+            <h1>Hello Tailwind!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Changed to include .jsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
