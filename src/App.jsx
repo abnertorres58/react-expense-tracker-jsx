67e4863b84a961e9fc410ff154f7cb758972016d
unknown <abnertorres58@gmail.com>
2025-04-25 21:08:30 -0400

    Balance component finally ready and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css'; // You can delete this if you're not using it
+import './App.css';
+import Balance from "./components/Balance/Balance.jsx"; // You can delete this if you're not using it
 
 function App() {
     return (
-        <div className="bg-red-500 p-5 text-white">
-            <h1>Hello Tailwind!</h1>
+        <div className="p-5">
+            <div className="max-w-screen-lg mx-auto">
+                <Balance />
+            </div>
         </div>
     );
 }
---
+++ src/components/Balance/Balance.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Plus} from "lucide-react";
+
+const Balance = () => {
+    return (
+        <div className="bg-white p-5 rounded-lg container mx-auto my-5">
+        <h1 className="text-2xl font-bold">Your Balance</h1>
+        <div className="flex justify-between items-center">
+            <div className="">
+                <h2 className="mt-3 text-green-500 font-extrabold text-4xl">0.00</h2>
+                <div className="flex gap-4 mt-2">
+                    <p className="text-green-500">Income: 0.00</p>
+                    <p className="text-red-500">Expense: 0.00</p>
+                </div>
+            </div>
+            <button className="bg-green-500 text-white px-4 py-2 rounded-3xl hover:bg-green-600
+            hover:scale-110 transition-all duration-300 flex gap-2 items-center">
+                <Plus className="w-6 h-6"/>Add Transaction
+            </button>
+        </div>
+    </div>
+    );
+};
+
+export default Balance;
